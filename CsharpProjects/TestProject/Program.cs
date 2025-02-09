fb06668a3a3115321ad0888d481a056c1391d556
Full-Stack-Atok <komsaykerby@gmail.com>
2025-02-10 06:47:12 +0800

    Learned Switch Case

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -303,19 +303,100 @@
 // else 
 //     Console.WriteLine("Found chuck");
 
-int[] numbers = {4, 8, 15, 16, 23, 42};
-int total = 0;
-bool found = false;
-foreach (int number in numbers) {    total += number;
+// int[] numbers = {4, 8, 15, 16, 23, 42};
+// int total = 0;
+// bool found = false;
+// foreach (int number in numbers) {    total += number;
 
-    if (number == 42) {
-        found = true;
-    }
+//     if (number == 42) {
+//         found = true;
+//     }
     
-}
+// }
 
-if (found) {
-    Console.WriteLine("Set contains 42");
-}
+// if (found) {
+//     Console.WriteLine("Set contains 42");
+// }
 
-Console.WriteLine($"Total: {total}");
+// Console.WriteLine($"Total: {total}");
+
+// Branch the flow of code using switch-case construct in C#
+
+// int employeeLevel = 100;
+// string employeeName = "John Smith";
+
+// string title = "";
+
+// switch (employeeLevel) {
+
+//     case 100:
+//     case 200:
+//         title = "Senior Associate";
+//         break;
+//     case 300:
+//         title = "Manager";
+//         break;
+//     case 400:
+//         title = "Senior Manager";
+//         break;
+//     default:
+//         title = "Associate";
+//         break;    
+// }
+
+// Console.WriteLine($"{employeeName}, {title}");
+
+// SKU = Stock Keeping Unit
+// SKU value format: <product #>-<2-letter color code>-<size code>
+// string sku = "01-MN-L";
+
+// string[] product = sku.Split('-');
+
+// string type = "";
+// string color = "";
+// string size = "";
+
+// switch (product[0]) {
+//     case "01":
+//         type = "Sweatshirt";
+//         break;
+//     case "02":
+//         type = "T-shirt";
+//         break;
+//     case "03":
+//         type = "Sweat pants";
+//         break;
+//     default:
+//         type = "Other";
+//         break;
+// }
+
+// switch (product[1]) {
+//     case "BL":
+//         color = "Black";
+//         break;
+//     case "MN":
+//         color = "Maroon";
+//         break;
+//     default:
+//         color = "White";
+//         break;    
+// }
+
+// switch (product[2]) {
+//     case "S":
+//         size = "Small";
+//         break;
+//     case "M":
+//         size = "Medium";
+//         break;
+//     case "L":
+//         size = "Large";
+//         break;
+//     default:
+//         size = "One size fits all";
+//         break;
+// }
+
+// Console.WriteLine($"Product: {size} {color} {type}");
+    
--- CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55b5923b673652f327db354df47179fb16f204a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d863d3b77689987feaa5e238c17e604f9faf49ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
