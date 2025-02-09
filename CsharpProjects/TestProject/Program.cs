877d2a053ecc8df9f5881b4724cc647046003f96
Full-Stack-Atok <komsaykerby@gmail.com>
2025-02-10 07:41:58 +0800

    Learned for loop iteration

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -400,3 +400,38 @@
 
 // Console.WriteLine($"Product: {size} {color} {type}");
     
+// Iterate through code block using for statement in C#
+
+// for (int i = 0; i < 10; i++) {
+//     Console.WriteLine(i);
+//     if (i == 7) {
+//         break;
+//     }
+// }
+
+// string[] names = {"Alex", "Eddie" , "David", "Michael"};
+
+// for (int i = 0; i < names.Length; i++) {
+//     if (names[i] == "David") names[i] = "Sammy";
+// }
+
+// foreach (var name in names) {
+//     Console.WriteLine(name);
+// }
+
+// FizzBuss
+
+// for (int i = 1; i <= 100; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         Console.WriteLine($"{i} - FizzBuzz");
+//     }
+//     else if (i % 3 == 0) {
+//         Console.WriteLine($"{i} - Fizz");
+//     }
+//     else if (i % 5 == 0) {
+//         Console.WriteLine($"{i} - Buzz");
+//     }
+//     else {
+//         Console.WriteLine(i);
+//     }
+// }
