a204008c50f01367afb91ce1bd8101bd9c5bdef7
Full-Stack-Atok <komsaykerby@gmail.com>
2025-02-25 21:13:05 +0800

    Learned Parameters & Methods

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1340,52 +1340,111 @@ else ipAddress in invalid
 //     Console.WriteLine($"Middle: {status}");
 // }
 
-string[] guestList = {"Rebecca", "Nadia", "Noor", "Jonte"};
-string[] rsvps = new string[10];
-int count = 0;
+// string[] guestList = {"Rebecca", "Nadia", "Noor", "Jonte"};
+// string[] rsvps = new string[10];
+// int count = 0;
 
-RSVP("Rebecca");
-RSVP("Nadia", 2, "Nuts");
-RSVP(name: "Linh", partySize: 2, allergies: "none", inviteOnly: false);
-RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
-RSVP("Noor", 4, inviteOnly: false);
-RSVP("Jonte", 2, "Stone fruit", false);
+// RSVP("Rebecca");
+// RSVP("Nadia", 2, "Nuts");
+// RSVP(name: "Linh", partySize: 2, allergies: "none", inviteOnly: false);
+// RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
+// RSVP("Noor", 4, inviteOnly: false);
+// RSVP("Jonte", 2, "Stone fruit", false);
 
-ShowRSVPs();
+// ShowRSVPs();
 
-void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
+// void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
+// {
+//     if (inviteOnly)
+//     {
+//         // search guestList before adding rsvp
+//         if (inviteOnly)
+//         {
+//             bool found = false;
+//             foreach (string guest in guestList)
+//             {
+//                 if (guest.Equals(name))
+//                 {
+//                     found = true;
+//                     break;
+//                 }
+//             }
+//             if (!found)
+//             {
+//                 Console.WriteLine($"Sorry, {name} is not on the guest list.");
+//                 return;
+//             }
+//         }
+//     }
+
+//     rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+//     count++;
+// }
+
+// void ShowRSVPs()
+// {
+//     Console.WriteLine("\nTotal RSVPs:");
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.WriteLine(rsvps[i]);
+//     }
+// }
+
+string[,] corporate = 
 {
-    if (inviteOnly)
-    {
-        // search guestList before adding rsvp
-        if (inviteOnly)
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+CorporateEmail(corporate);
+ExternalEmail(external);
+
+
+
+void CorporateEmail(string[,] employees)
+{ 
+    for (int i = 0; i < corporate.GetLength(0); i++)
+    {   
+        string firstName = employees[i, 0];
+        String lastName = employees[i, 1];
+
+        if (firstName.Length >=2 )
         {
-            bool found = false;
-            foreach (string guest in guestList)
-            {
-                if (guest.Equals(name))
-                {
-                    found = true;
-                    break;
-                }
-            }
-            if (!found)
-            {
-                Console.WriteLine($"Sorry, {name} is not on the guest list.");
-                return;
-            }
+            string email = $"{firstName.Substring(0, 2).ToLower()}{lastName.ToLower()}@contoso.com";
+            Console.WriteLine(email);
+        }
+        else
+        {
+            Console.WriteLine($"Error: First name '{firstName}' is too short to generate an email");
         }
     }
-
-    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
-    count++;
 }
 
-void ShowRSVPs()
+void ExternalEmail(string[,] employees)
 {
-    Console.WriteLine("\nTotal RSVPs:");
-    for (int i = 0; i < count; i++)
+    for (int i = 0; i < external.GetLength(0); i++)
     {
-        Console.WriteLine(rsvps[i]);
+        string firstName = external[i, 0];
+        string lastName = external[i, 1];
+
+        if (firstName.Length >= 2)
+        {
+            string email = $"{firstName.Substring(0, 2).ToLower()}{lastName.ToLower()}@{externalDomain}";
+            Console.WriteLine(email);
+        }
+        else
+        {
+            Console.WriteLine($"Error: First name '{firstName}' is too short to generate an email");
+        }
+        
     }
 }
