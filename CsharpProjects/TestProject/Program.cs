fc9213caba09d133523fda28e5efb626baa9e6fe
Full-Stack-Atok <komsaykerby@gmail.com>
2025-03-04 21:02:19 +0800

    Finish part 5

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1451,35 +1451,410 @@ else ipAddress in invalid
 
 //Create C# methods that return values
 
-double total = 0;
-double minimumSpend = 30.00;
+// double total = 0;
+// double minimumSpend = 30.00;
 
-double[] items = { 15.97, 3.50, 12.25, 22.99, 10.98 };
-double[] discounts = { 0.30, 0.00, 0.10, 0.20, 0.50 };
+// double[] items = { 15.97, 3.50, 12.25, 22.99, 10.98 };
+// double[] discounts = { 0.30, 0.00, 0.10, 0.20, 0.50 };
 
-for (int i = 0; i < items.Length; i++)
+// for (int i = 0; i < items.Length; i++)
+// {
+//     total += GetDiscountedPrice(i);
+// }
+
+// total -= TotalMeetsMinimum() ? 5.00 : 0;
+
+// Console.WriteLine($"Total: ${FormatDecimal(total)}");
+
+// double GetDiscountedPrice(int itemIndex)
+// {
+//     // Calculate the discounted price of the item
+//     return items[itemIndex] * (1 - discounts[itemIndex]);
+// }
+
+// bool TotalMeetsMinimum()
+// {
+//     // Check if the total meets the minimum
+//     return total >= minimumSpend;
+// }
+
+// string FormatDecimal(double input)
+// {
+//     // Format the double so only 2 decimal places are displayed
+//     return input.ToString().Substring(0, 5);
+// }
+
+// double usd = 23.73;
+// int vnd = UsdToVnd(usd);
+
+// Console.WriteLine($"${usd} USD = ${vnd} VND");
+// Console.WriteLine($"${vnd} VND = ${VndToUsd(vnd)} USD");
+
+// int UsdToVnd(double usd)
+// {
+//     int rate = 23500;
+//     return (int)(rate * usd);
+// }
+
+// double VndToUsd(int vnd)
+// {
+//     double rate = 23500;
+//     return vnd / rate;
+// }
+
+// string ReverseSentence(string input)
+// {
+//     string result ="";
+//     string[] words = input.Split(" ");
+
+//     foreach (string word in words)
+//     {
+//         result += ReverseWord(word) + " ";
+//     }
+
+//     return result.Trim();
+// }
+
+// string ReverseWord(string word)
+// {
+//     string result = "";
+
+//     for (int i = word.Length - 1; i >= 0; i--)
+//     {
+//         result += word[i];
+//     }
+//     return result;
+// }
+
+// string input = "There are snake at the zoo";
+
+// Console.WriteLine(input);
+// Console.WriteLine(ReverseSentence(input));
+
+// string[] words = {"racecar", "talented", "deified", "tent", "tenet"};
+
+// Console.WriteLine("Is it a palindrome?");
+// foreach(string word in words)
+// {
+//     Console.WriteLine($"{word}: {IsPalindrome(word)}");
+// }
+
+// bool IsPalindrome(string word)
+// {
+//     int start = 0;
+//     int end = word.Length - 1;
+
+//     while(start < end)
+//     {
+//         if(word[start] != word[end])
+//         {
+//             return false;
+//         }
+//         start++;
+//         end--;
+//     }
+//     return true;
+// }
+
+// int target = 80;
+// int[] coins = new int[] { 5, 5, 50, 25, 25, 10, 5 };
+// int[,] result = TwoCoins(coins, target);
+
+// if (result.Length == 0)
+// {
+//     Console.WriteLine("No two coins make change");
+// }
+// else
+// {
+//     Console.WriteLine($"Change found at positions:");
+//     for (int i = 0; i < result.GetLength(0); i++)
+//     {
+//         if(result[i, 0] == -1)
+//         {
+//             break;
+//         }
+//         Console.WriteLine($"{result[i, 0]}, {result[i, 1]}");
+//     }
+// }
+
+// int[,] TwoCoins(int[] coins, int target)
+// {
+//     int[,] result = { { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 }, { -1, -1 } };
+//     int count = 0;
+//     for (int curr = 0; curr < coins.Length; curr++)
+//     {
+//         for (int next = curr + 1; next < coins.Length; next++)
+//         {
+//             if (coins[curr] + coins[next] == target)
+//             {
+//                result[count, 0] = curr;
+//                result[count, 1] = next;
+//                count++;
+//             }
+
+//             if(count == result.GetLength(0))
+//             {
+//                 return result;
+//             }
+//         }
+//     }
+//     return (count == 0) ? new int[0, 0] : result;
+// }
+
+// Random random = new Random();
+// Console.WriteLine("Would you like to play? (Y/N)");
+
+// if (ShouldPlay()) 
+// {
+//     PlayGame();
+// }
+
+// void PlayGame() 
+// {
+//     var play = true;
+
+//     while (play) 
+//     {
+//         var target = random.Next(1, 6);
+//         var roll = random.Next(1, 7);
+
+//         Console.WriteLine($"Roll a number greater than {target} to win!");
+//         Console.WriteLine($"You rolled a {roll}");
+//         Console.WriteLine(WinOrLose(target, roll));
+//         Console.WriteLine("\nPlay again? (Y/N)");
+
+//         play = ShouldPlay();
+//     }
+// }
+
+// bool ShouldPlay(){
+//     string input = (Console.ReadLine()?? "").Trim().ToLower();
+//     return input == "yes" || input == "y";
+// }
+
+// string WinOrLose(int target, int roll)
+// {
+//     return roll > target? "🎉 You win!\n" : "❌You lose!\n";
+// }
+
+// string[] pettingZoo =
+// {
+//     "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+//     "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
+//     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+// };
+
+// PlanSchoolVisit("School A");
+// PlanSchoolVisit("School B", 3);
+// PlanSchoolVisit("School C", 2);
+
+
+
+// void PlanSchoolVisit(string schoolName, int groups = 6)
+// {
+//     RandomizeAnimals();
+//     string[,] group1 = AssignGroup(groups);
+//     Console.WriteLine(schoolName);
+//     PrintGroup(group1);
+// }
+
+
+// void RandomizeAnimals()
+// {
+//     Random random = new Random();
+
+//     for (int i = 0; i < pettingZoo.Length; i++)
+//     {
+//         int r = random.Next(i, pettingZoo.Length);
+
+//         string temp = pettingZoo[i];
+//         pettingZoo[i] = pettingZoo[r];
+//         pettingZoo[r] = temp;
+//     }
+
+// }
+// string[,] AssignGroup(int groups = 6)
+// {
+//     string[,] result = new string[groups, pettingZoo.Length / groups];
+
+//     int start = 0;
+//     for (int i = 0; i < groups; i++)
+//     {
+//         for (int j = 0; j < result.GetLength(1); j++)
+//         {
+//             result[i, j] = pettingZoo[start++];
+//         }
+//     }
+//     return result;
+// }
+
+// void PrintGroup(string[,] group)
+// {
+//     for (int i = 0; i < group.GetLength(0); i++)
+//     {
+//         Console.Write($"Group {i + 1}: ");
+//         for (int j = 0; j < group.GetLength(1); j++)
+//         {
+//             Console.Write($"{group[i, j]}  ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+Random random = new Random();
+Console.CursorVisible = false;
+int height = Console.WindowHeight - 1;
+int width = Console.WindowWidth - 5;
+bool shouldExit = false;
+
+// Console position of the player
+int playerX = 0;
+int playerY = 0;
+
+// Console position of the food
+int foodX = 0;
+int foodY = 0;
+
+// Available player and food strings
+string[] states = { "('-')", "(^-^)", "(X_X)" };
+string[] foods = { "@@@@@", "$$$$$", "#####" };
+
+// Current player string displayed in the Console
+string player = states[0];
+
+// Index of the current food
+int food = 0;
+
+InitializeGame();
+while (!shouldExit)
 {
-    total += GetDiscountedPrice(i);
+    if (TerminalResized())
+    {
+        Console.Clear();
+        Console.WriteLine("Console was resized. Program exiting.");
+        break;
+    }
+    Move();
 }
 
-total -= TotalMeetsMinimum() ? 5.00 : 0;
-
-Console.WriteLine($"Total: ${FormatDecimal(total)}");
-
-double GetDiscountedPrice(int itemIndex)
+// Returns true if the Terminal was resized 
+bool TerminalResized()
 {
-    // Calculate the discounted price of the item
-    return items[itemIndex] * (1 - discounts[itemIndex]);
+    return height != Console.WindowHeight - 1 || width != Console.WindowWidth - 5;
 }
 
-bool TotalMeetsMinimum()
+// Displays random food at a random location
+void ShowFood()
 {
-    // Check if the total meets the minimum
-    return total >= minimumSpend;
+    // Update food to a random index
+    food = random.Next(0, foods.Length);
+
+    // Update food position to a random location
+    foodX = random.Next(0, width - player.Length);
+    foodY = random.Next(0, height - 1);
+
+    // Display the food at the location
+    Console.SetCursorPosition(foodX, foodY);
+    Console.Write(foods[food]);
 }
 
-string FormatDecimal(double input)
+bool ShoulFreezePlayer()
 {
-    // Format the double so only 2 decimal places are displayed
-    return input.ToString().Substring(0, 5);
+    return player == states[2];
+}
+
+bool ShouldIncreaseSpeed()
+{
+    return player == states[1];
+}
+
+// Changes the player to match the food consumed
+void ChangePlayer()
+{
+    player = states[food];
+    Console.SetCursorPosition(playerX, playerY);
+    Console.Write(player);
+}
+
+// Temporarily stops the player from moving
+void FreezePlayer()
+{
+    System.Threading.Thread.Sleep(1000);
+    player = states[0];
+}
+
+bool PlayerAteFood()
+{
+    return playerX == foodX && playerY == foodY;
+}
+
+// Reads directional input from the Console and moves the player
+void Move(bool allowOptionalTermination = false, int speed = 1)
+{
+
+    if(ShoulFreezePlayer())
+    {
+        FreezePlayer();
+    }
+    int lastX = playerX;
+    int lastY = playerY;
+
+    ConsoleKey key = Console.ReadKey(true).Key;
+
+    int moveSpeed = ShouldIncreaseSpeed() ? 3 : speed;
+
+    switch (key)
+    {
+        case ConsoleKey.UpArrow:
+            playerY--;
+            break;
+        case ConsoleKey.DownArrow:
+            playerY++;
+            break;
+        case ConsoleKey.LeftArrow:
+            playerX--;
+            break;
+        case ConsoleKey.RightArrow:
+            playerX++;
+            break;
+        case ConsoleKey.Escape:
+            shouldExit = true;
+            break;
+        default:
+            if (allowOptionalTermination)
+            {
+                shouldExit = true;
+                return;
+            }
+            break;
+    }
+
+    // Clear the characters at the previous position
+    Console.SetCursorPosition(lastX, lastY);
+    for (int i = 0; i < player.Length; i++)
+    {
+        Console.Write(" ");
+    }
+
+    // Keep player position within the bounds of the Terminal window
+    playerX = Math.Clamp(playerX, 0, width);
+    playerY = Math.Clamp(playerY, 0, height);
+
+    // Draw the player at the new location
+    Console.SetCursorPosition(playerX, playerY);
+    Console.Write(player);
+
+    if(PlayerAteFood()){
+        ChangePlayer();
+        ShowFood();
+    }
+}
+
+// Clears the console, displays the food and player
+void InitializeGame()
+{
+    Console.Clear();
+    ShowFood();
+    Console.SetCursorPosition(0, 0);
+    Console.Write(player);
 }
--- CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bec398a996abacecf001ba8e6ac0351a8ecf4754")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db07e3694aa7547c83e3b38bcb4a393620d7874d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
